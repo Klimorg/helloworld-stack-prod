5d170de38b3f637594e00e79de32fcc7d91186b8
vorph <klimczak.mathieu@pm.me>
2022-03-10 17:55:43 +0100

    Test db part to api

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,7 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+# from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
 
 from .config import settings
 
