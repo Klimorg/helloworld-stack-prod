3ddb13485376893e3161bf45e4b12aab52bd16cc
vorph <klimczak.mathieu@pm.me>
2022-03-17 11:59:50 +0100

    Add redirection route

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    db_user: str = Field(default="postgres", env="DB_USER")
+    db_user: str = Field(default="postgres", env="DB_USERNAME")
     db_pwd: str = Field(default="postgres", env="DB_PASSWORD")
     db_host: str = Field(default="db", env="DB_HOST")
     db_port: str = Field(default="5432", env="DB_PORT")
